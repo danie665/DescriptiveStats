bfc9b9f0b06191003706d86d017c97e93881f045
goldy <danie665@umn.edu>
2018-03-12 23:22:32 -0500

    this needs a ton of work

--- TaylorSet.py
+++ TaylorSet.py
@@ -13,14 +13,15 @@ class TaylorSet():
             i += 1
         return minimum
     def maximum(self):
-        maximum = self.data [0]
+        maximum = self.data[0]
         i = 1
         while i < len(self.data):
             if maximum > self.data[i]:
                 maximum = self.data[i]
             i += 1
         return maximum
-    def average(self):
+    def mean(self):
+        """ returns mean from an array """
         count = 0
         total = 0
         i = 0
@@ -29,4 +30,26 @@ class TaylorSet():
             total += self.data[i]
             i += 1
         return total / count
+    def median(self):
+        """ returns the median from an array """
+        temp_array = self.data
+        sorted_array
+        half = (len(self.data) + 1)
+        if half % 2 == 0:
+            index = half / 2
+            return self.data[index]
+        else:
+            lower_index = (half / 2) - .5
+            upper_index = (half / 2) + .5
+            average = (self.data[upper_index] + self.data[lower_index]) / 2
+            return average
+    def weighted_mean(self):
+        """ returns weighted mean from an array of arrays """
+        weighted_mean = 0
+        count = 0
+        i = 0
+        while i < len(self.data):
+            weighted_mean += self.data[i][0] * self.data[i][1]
+            i += 1
+        return weighted_mean
 
--- TaylorSetTests.py
+++ TaylorSetTests.py
@@ -2,10 +2,13 @@ import unittest
 
 from TaylorSet import TaylorSet
 
-class MaximumTestCaseOne(unittest.TestCase):
-    def test_basic(self):
-        my_data = [ 1, 2, 3, 4, 5 ]
+class MeanAndMedian(unittest.TestCase):
+    def method_one_mean(self):
+        my_data = [ 10, 20, 12, 17, 16 ]
         my_set = TaylorSet(my_data)
-        self.assertEqual(my_set.average(), 3)
-
+        self.assertEqual(my_set.mean(), 15)
+    def method_one_median(self):
+        my_data = [ 10, 20, 12, 17, 16 ]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(myset
 unittest.main()

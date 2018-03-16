b8b6deb18ce09353e0ac1992cf92711b90eb4159
danie665 <danie665@umn.edu>
2018-03-16 18:20:05 -0500

    adding today's updates

--- TaylorSet.py
+++ TaylorSet.py
@@ -1,53 +1,20 @@
-
 class TaylorSet():
     """ a simple descriptive statistics model """
     def __init__(self, data):
-        """ initialize data attributes """
+        """ initialize data """
         self.data = data
-    def minimum(self):
-        """ returns the minimum value of an array """
-        minimum = self.data[0]
-        i = 1
-        while i < len(self.data):
-            if minimum < self.data[i]:
-                minimum = self.data[i]
-            i += 1
-        return minimum
-    def maximum(self):
-        """ returns the maximum value of an array """
-        maximum = self.data[0]
-        i = 1
-        while i < len(self.data):
-            if maximum > self.data[i]:
-                maximum = self.data[i]
-            i += 1
-        return maximum
+        self.data = [float(x) for x in self.data]
+        self.data.sort()
+    def mean(self):
+        """ returns the arithmetic mean of an array """
+        return sum(self.data) / len(self.data)
     def median(self):
-        """ returns the median from an array """
-        temp_array = self.data
-        sorted_array
-        half = (len(self.data) + 1)
-        if half % 2 == 0:
-            index = half / 2 
-            return self.data[index]
-        else:
-            lower_index = (half / 2) - .5
-            upper_index = (half / 2) + .5
-            average = (self.data[upper_index] + self.data[lower_index]) / 2 
-            return average
+        """ returns the median of an array """
+        place = len(self.data) / 2
+        return(place)
     def range(self):
         """ return the range of the data """
         return 0
-    def mean(self):
-        """ returns mean from an array """
-        count = 0
-        total = 0
-        i = 0
-        while i < len(self.data):
-            count += 1
-            total += self.data[i]
-            i += 1
-        return total / count
     def weighted_mean(self):
         """ returns weighted mean from an array of arrays """
         weighted_mean = 0
--- TaylorSetTests.py
+++ TaylorSetTests.py
@@ -1,14 +1,32 @@
 import unittest
-
 from TaylorSet import TaylorSet
 
-class MeanAndMedian(unittest.TestCase):
-    def method_one_mean(self):
+class ConvertToFloatFailures(unittest.TestCase):
+    @unittest.expectedFailure
+    def test_strings_convert(self):
+        # ensure string to float converion fails
+        my_data = [ "One", "Two", "Three" ]
+        my_set = TaylorSet(my_data)
+
+class BuiltIns(unittest.TestCase):
+    def test_minimum(self):
+        my_data = [ 10, 20, 12, 17, 16 ]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(min(my_set.data), 10)
+    def test_maximum(self):
+        my_data = [ 10, 20, 12, 17, 16 ]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(max(my_set.data), 20)
+
+class Center(unittest.TestCase):
+    def test_mean(self):
         my_data = [ 10, 20, 12, 17, 16 ]
         my_set = TaylorSet(my_data)
         self.assertEqual(my_set.mean(), 15)
-    def method_one_median(self):
+    def test_median(self):
         my_data = [ 10, 20, 12, 17, 16 ]
         my_set = TaylorSet(my_data)
-        self.assertEqual(myset
+        print(my_set.median())
+
+
 unittest.main()

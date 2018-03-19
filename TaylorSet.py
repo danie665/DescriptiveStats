11f741f196c66d50674394b5d5559e83fcc15647
danie665 <danie665@umn.edu>
2018-03-19 17:47:50 -0500

    all of the updates

--- TaylorSet.py
+++ TaylorSet.py
@@ -1,62 +1,114 @@
 class TaylorSet():
     """ a simple descriptive statistics model """
+
     def __init__(self, data):
         """ initialize data """
         self.data = data
         self.data = [float(x) for x in self.data]
-        self.data.sort()
+
     def mean(self):
         """ returns the arithmetic mean of an array """
         return sum(self.data) / len(self.data)
+
     def median(self):
         """ returns the median of an array """
-        place = len(self.data) / 2
-        return(place)
+        sorted_array = sorted(self.data)
+        if len(sorted_array) % 2 == 0:
+            lower = int( ( len(sorted_array) / 2 ) )
+            upper = int( ( len(sorted_array) / 2 ) + 1 )
+            return ( sorted_array[lower-1] + sorted_array[upper-1] ) / 2
+        else:
+            place = int( ( len(sorted_array) + 1 ) / 2 )
+            return sorted_array[place-1]
+        print(self.data)
+
     def range(self):
         """ return the range of the data """
-        return 0
+        return max(self.data) - min(self.data)
+
     def weighted_mean(self):
         """ returns weighted mean from an array of arrays """
-        weighted_mean = 0
-        count = 0
+        return 0
+
+    def geometric_mean(self):
+        """ returns geometic mean of an array """
+        my_product = 1
         i = 0
         while i < len(self.data):
-            weighted_mean += self.data[i][0] * self.data[i][1]
+            my_product *= self.data[i]
             i += 1
-        return weighted_mean
-    def geometic_mean(self):
-        """ returns geometic mean of an array """
-        return 0
-    def percentile_location(self):
+        return my_product ** ( 1 / len(self.data) )
+
+    def percentile(self, percentile):
         """ returns the percentile location of an array """
-        return 0
+        sorted_array = sorted(self.data)
+        location = ( len(sorted_array) + 1 ) * percentile
+        lower = int(location) - 1
+        upper = int(location)
+        mid_distance = location - int(location)
+        return ( sorted_array[lower] +
+                (( sorted_array[upper] - sorted_array[lower] ) * mid_distance ) )
+
     def interquartile_range(self):
         """ returns the interquartile range of an array """
         return 0
+
     def population_variance(self):
         """ returns population variance of an array """
-        return 0
+        my_mean = self.mean()
+        my_sum = 0
+        i = 0
+        while i < len(self.data):
+            my_sum += ( self.data[i] - my_mean ) ** 2
+            i += 1
+        return my_sum / len(self.data)
+
     def sample_variance(self):
         """ returns sample variance of an array """
-        return 0
-    def standard_deviation(self):
-        """ returns the standard deviation of an array """
-        return 0
-    def coefficient_of_variation(self):
-        """ returns the coefficient of variation of an array """
-        return 0
-    def z_score(self):
-        """ returns the z-score of an element in an array """
-        return 0
+        my_mean = self.mean()
+        my_sum = 0
+        i = 0
+        while i < len(self.data):
+            my_sum += ( self.data[i] - my_mean ) ** 2
+            i += 1
+        return my_sum / ( len(self.data) - 1 )
+
+    def population_standard_deviation(self):
+        """ returns the population standard deviation of an array """
+        return self.population_variance() ** 0.5
+
+    def sample_standard_deviation(self):
+        """ returns the sample standard deviation of an array """
+        return self.sample_variance() ** 0.5
+
+    def population_coefficient_of_variation(self):
+        """ returns the population coefficient of variation of an array """
+        return self.population_standard_deviation() / self.mean()
+
+    def sample_coefficient_of_variation(self):
+        """ returns the sample coefficient of variation of an array """
+        return self.sample_standard_deviation() / self.mean()
+
+    def population_z_score(self, position):
+        """ returns the population z-score of an element in an array """
+        return ( self.data[position] - self.mean() ) / self.population_standard_deviation()
+
+    def sample_z_score(self, position):
+        """ returns the sample z-score or an element in a list """
+        return ( self.data[position] - self.mean() ) / self.sample_standard_deviation()
+
     def sample_covariance(self):
         """ returns the sample covariance of an array """
         return 0
+
     def population_covariance(self):
         """ returns the population covariance of an array """
         return 0
+
     def pearson_sample(self):
         """ returns the pearson product moment correlation coefficient for sample data """
         return 0
+
     def pearson_population(self):
         """ returns the pearson product moment correlation coefficient for population data """
         return 0
--- TaylorSetTests.py
+++ TaylorSetTests.py
@@ -23,10 +23,61 @@ class Center(unittest.TestCase):
         my_data = [ 10, 20, 12, 17, 16 ]
         my_set = TaylorSet(my_data)
         self.assertEqual(my_set.mean(), 15)
-    def test_median(self):
+    def test_median_odd(self):
         my_data = [ 10, 20, 12, 17, 16 ]
         my_set = TaylorSet(my_data)
-        print(my_set.median())
+        self.assertEqual(my_set.median(), 16)
+    def test_median_even(self):
+        my_data = [ 10, 20, 12, 17, 16, 14 ]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(my_set.median(), 15)
+    def test_geometic_mean(self):
+        my_data = [ 1.10, 1.15, 1.21, 0.51, 1.05 ]
+        my_set = TaylorSet(my_data)
+        self.assertAlmostEqual(my_set.geometric_mean(), .9610, 4)
 
+class Dispersion(unittest.TestCase):
+    def test_range(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(my_set.range(), 10)
+    def test_population_variance(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(my_set.population_variance(), 12.8)
+    def test_sample_variance(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(my_set.sample_variance(), 16)
+    def test_population_standard_deviation(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertAlmostEqual(my_set.population_standard_deviation(), 3.5777, 4)
+    def test_sample_standard_deviation(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(my_set.sample_standard_deviation(), 4)
+    def test_population_coefficient_of_variation(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertAlmostEqual(my_set.population_coefficient_of_variation(), .2385, 4)
+    def test_sample_coefficient_of_variation(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertAlmostEqual(my_set.sample_coefficient_of_variation(), .2667, 4)
+    def test_percentile(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(my_set.percentile(.25), 11)
+
+class ZScore(unittest.TestCase):
+    def test_population_z_score(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertAlmostEqual(my_set.population_z_score(0), -1.3975, 4)
+    def test_sample_z_score(self):
+        my_data = [10, 20, 12, 17, 16]
+        my_set = TaylorSet(my_data)
+        self.assertEqual(my_set.sample_z_score(0), -1.25)
 
 unittest.main()

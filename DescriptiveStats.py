6af16bfbbdb0e111d483c4d395c9963b35dd10ab
Taylor Danielson <danie665@umn.edu>
2019-06-10 22:30:38 -0700

    Getting ready for regression

--- DescriptiveStats.py
+++ DescriptiveStats.py
@@ -2,10 +2,10 @@ class DescriptiveStats():
 
     """ a simple descriptive statistics model """
 
-    def __init__(self, data, data_2=[], data_3=[], data_4=[], data_5=[]):
+    def __init__(self, data_1=[], data_2=[], data_3=[], data_4=[], data_5=[]):
         """ initialize data """
-        self.data = data
-        self.data = [float(x) for x in self.data]
+        self.data_1 = data_1
+        self.data_1 = [float(x) for x in self.data_1]
         self.data_2 = data_2
         self.data_2 = [float(x) for x in self.data_2]
         self.data_3 = data_3
@@ -17,7 +17,7 @@ class DescriptiveStats():
 
     def _lists_uniform(self):
         """ ensures that both input lists are the same length """
-        length = len(data)
+        length = len(self.data_1)
         if (
                 (len(data_2) == length or len(data_2) == 0) and
                 (len(data_3) == length or len(data_3) == 0) and
@@ -27,13 +27,16 @@ class DescriptiveStats():
             return True
         return False
 
-    def mean(self):
+    def mean(self, data=[]):
         """ returns the arithmetic mean of an array """
-        return sum(self.data) / len(self.data)
+        if len(data) == 0:
+            return sum(data) / len(data)
+        else:
+            return sum(data) / len(data)
 
-    def median(self):
+    def median(self, data=[]):
         """ returns the median of an array """
-        sorted_array = sorted(self.data)
+        sorted_array = sorted(data)
         if len(sorted_array) % 2 == 0:
             lower = int( ( len(sorted_array) / 2 ) )
             upper = int( ( len(sorted_array) / 2 ) + 1 )
@@ -42,15 +45,15 @@ class DescriptiveStats():
             place = int( ( len(sorted_array) + 1 ) / 2 )
             return sorted_array[place-1]
 
-    def mode(self):
+    def mode(self, data=[]):
         """ return the mode of the data """
         tally_dict = {}
         i = 0
-        while i < len(self.data):
-            if self.data[i] in tally_dict:
-                tally_dict[self.data[i]] += 1
+        while i < len(data):
+            if data[i] in tally_dict:
+                tally_dict[data[i]] += 1
             else:
-                tally_dict[self.data[i]] = 1
+                tally_dict[data[i]] = 1
             i += 1
         max_occ = 0
         for key, value in tally_dict.items():
@@ -65,9 +68,9 @@ class DescriptiveStats():
         else:
             return modes
 
-    def range(self):
+    def range(self, data=[]):
         """ return the range of the data """
-        return max(self.data) - min(self.data)
+        return max(data) - min(data)
 
     def weighted_mean(self):
         """ returns weighted mean from an array of arrays """
@@ -75,18 +78,18 @@ class DescriptiveStats():
             raise ValueError
         return 0
 
-    def geometric_mean(self):
+    def geometric_mean(self, data=[]):
         """ returns geometic mean of an array """
         my_product = 1
         i = 0
-        while i < len(self.data):
-            my_product *= self.data[i]
+        while i < len(data):
+            my_product *= data[i]
             i += 1
-        return my_product ** ( 1 / len(self.data) )
+        return my_product ** ( 1 / len(data) )
 
-    def percentile(self, percentile):
+    def percentile(self, percentile, data=[]):
         """ returns the percentile location of an array """
-        sorted_array = sorted(self.data)
+        sorted_array = sorted(data)
         location = ( len(sorted_array) + 1 ) * percentile
         lower = int(location) - 1
         upper = int(location)
@@ -94,60 +97,68 @@ class DescriptiveStats():
         return ( sorted_array[lower] +
                 (( sorted_array[upper] - sorted_array[lower] ) * mid_distance ) )
 
-    def interquartile_range(self):
+    def interquartile_range(self, data=[]):
         """ returns the interquartile range of an array """
-        return self.percentile(.75) - self.percentile(.25)
+        return self.percentile(.75, data=data) - self.percentile(.25, data=data)
 
-    def population_variance(self):
+    def population_variance(self, data=[]):
         """ returns population variance of an array """
-        my_mean = self.mean()
+        my_mean = self.mean(data=data)
         my_sum = 0
         i = 0
-        while i < len(self.data):
-            my_sum += ( self.data[i] - my_mean ) ** 2
+        while i < len(data):
+            my_sum += ( data[i] - my_mean ) ** 2
             i += 1
-        return my_sum / len(self.data)
+        return my_sum / len(data)
 
-    def sample_variance(self):
+    def sample_variance(self, data=[]):
         """ returns sample variance of an array """
-        my_mean = self.mean()
+        my_mean = self.mean(data=data)
         my_sum = 0
         i = 0
-        while i < len(self.data):
-            my_sum += ( self.data[i] - my_mean ) ** 2
+        while i < len(data):
+            my_sum += ( data[i] - my_mean ) ** 2
             i += 1
-        return my_sum / ( len(self.data) - 1 )
+        return my_sum / ( len(data) - 1 )
 
-    def population_standard_deviation(self):
+    def population_standard_deviation(self, data=[]):
         """ returns the population standard deviation of an array """
-        return self.population_variance() ** 0.5
+        return self.population_variance(data=data) ** 0.5
 
-    def sample_standard_deviation(self):
+    def sample_standard_deviation(self, data=[]):
         """ returns the sample standard deviation of an array """
-        return self.sample_variance() ** 0.5
+        return self.sample_variance(data=data) ** 0.5
 
-    def population_coefficient_of_variation(self):
+    def population_coefficient_of_variation(self, data=[]):
         """ returns the population coefficient of variation of an array """
-        return self.population_standard_deviation() / self.mean()
+        return self.population_standard_deviation(data=data) / self.mean(data=data)
 
-    def sample_coefficient_of_variation(self):
+    def sample_coefficient_of_variation(self, data=[]):
         """ returns the sample coefficient of variation of an array """
-        return self.sample_standard_deviation() / self.mean()
+        return self.sample_standard_deviation(data=data) / self.mean(data=data)
 
-    def population_z_score(self, position):
+    def population_z_score(self, position, data=[]):
         """ returns the population z-score of an element in an array """
-        return ( self.data[position] - self.mean() ) / self.population_standard_deviation()
+        return ( ( data[position] - self.mean(data=data) ) / 
+                    self.population_standard_deviation(data=data) )
 
-    def sample_z_score(self, position):
+    def sample_z_score(self, position, data=[]):
         """ returns the sample z-score or an element in a list """
-        return ( self.data[position] - self.mean() ) / self.sample_standard_deviation()
+        return ( ( data[position] - self.mean(data=data) ) / 
+                    self.sample_standard_deviation(data=data) )
 
-    def sample_covariance(self):
+    def sample_covariance(self, data_1=[], data_2=[]):
         """ returns the sample covariance of an array """
+        if not _lists_uniform():
+            raise ValueError
+        my_mean_1 = self.mean(data=data_1)
+        my_mean_2 = self.mean(data=data_2)
+        my_covariance = 0
         i = 0
-        while (i < 
+        while (i < len(self.data_1)):
+            i += 1
         return 0
 
-    def population_covariance(self):
+    def population_covariance(self, data_1=[], data_2=[]):
         """ returns the population covariance of an array """
         return 0
--- DescriptiveStatsTests.py
+++ DescriptiveStatsTests.py
@@ -1,95 +1,96 @@
 import unittest
-import DescriptiveStats import DescriptiveStats
+from DescriptiveStats import DescriptiveStats
 
 class ConvertToFloatFailures(unittest.TestCase):
     @unittest.expectedFailure
     def test_strings_convert(self):
         # ensure string to float converion fails
         my_data = [ "One", "Two", "Three" ]
-        my_set = DescriptiveStats(my_data)
+        my_set = DescriptiveStats(data_1=my_data)
 
 class BuiltIns(unittest.TestCase):
     def test_minimum(self):
         my_data = [ 10, 20, 12, 17, 16 ]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(min(my_set.data), 10)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(min(my_set.data_1), 10)
     def test_maximum(self):
         my_data = [ 10, 20, 12, 17, 16 ]
         my_set = DescriptiveStats(my_data)
-        self.assertEqual(max(my_set.data), 20)
+        self.assertEqual(max(my_set.data_1), 20)
 
 class Center(unittest.TestCase):
     def test_mean(self):
         my_data = [ 10, 20, 12, 17, 16 ]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.mean(), 15)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.mean(my_set.data_1), 15)
     def test_median_odd(self):
         my_data = [ 10, 20, 12, 17, 16 ]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.median(), 16)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.median(my_set.data_1), 16)
     def test_median_even(self):
         my_data = [ 10, 20, 12, 17, 16, 14 ]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.median(), 15)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.median(my_set.data_1), 15)
     def test_geometic_mean(self):
         my_data = [ 1.10, 1.15, 1.21, 0.51, 1.05 ]
-        my_set = DescriptiveStats(my_data)
-        self.assertAlmostEqual(my_set.geometric_mean(), .9610, 4)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertAlmostEqual(my_set.geometric_mean(my_set.data_1), .9610, 4)
     def test_easy_mode(self):
         my_data = [ 1, 1, 2, 3 ]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.mode(), 1)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.mode(my_set.data_1), 1)
     def test_hard_mode(self):
         my_data = [ 1, 1, 2, 2, 3 ]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.mode(), [ 1, 2 ])
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.mode(my_set.data_1), [ 1, 2 ])
 
 class Dispersion(unittest.TestCase):
     def test_range(self):
         my_data = [10, 20, 12, 17, 16]
         my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.range(), 10)
+        self.assertEqual(my_set.range(my_set.data_1), 10)
     def test_population_variance(self):
         my_data = [10, 20, 12, 17, 16]
         my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.population_variance(), 12.8)
+        self.assertEqual(my_set.population_variance(my_set.data_1), 12.8)
     def test_sample_variance(self):
         my_data = [10, 20, 12, 17, 16]
         my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.sample_variance(), 16)
+        self.assertEqual(my_set.sample_variance(my_set.data_1), 16)
     def test_population_standard_deviation(self):
         my_data = [10, 20, 12, 17, 16]
         my_set = DescriptiveStats(my_data)
-        self.assertAlmostEqual(my_set.population_standard_deviation(), 3.5777, 4)
+        self.assertAlmostEqual(my_set.population_standard_deviation(my_set.data_1), 3.5777, 4)
     def test_sample_standard_deviation(self):
         my_data = [10, 20, 12, 17, 16]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.sample_standard_deviation(), 4)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.sample_standard_deviation(my_set.data_1), 4)
     def test_population_coefficient_of_variation(self):
         my_data = [10, 20, 12, 17, 16]
-        my_set = DescriptiveStats(my_data)
-        self.assertAlmostEqual(my_set.population_coefficient_of_variation(), .2385, 4)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertAlmostEqual(
+                my_set.population_coefficient_of_variation(my_set.data_1), .2385, 4)
     def test_sample_coefficient_of_variation(self):
         my_data = [10, 20, 12, 17, 16]
-        my_set = DescriptiveStats(my_data)
-        self.assertAlmostEqual(my_set.sample_coefficient_of_variation(), .2667, 4)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertAlmostEqual(my_set.sample_coefficient_of_variation(my_set.data_1), .2667, 4)
     def test_percentile(self):
         my_data = [10, 20, 12, 17, 16]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.percentile(.25), 11)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.percentile(.25, data=my_set.data_1), 11)
     def test_interquartile_range(self):
         my_data = [10, 20, 12, 17, 16]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.interquartile_range(), 7.5)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.interquartile_range(my_set.data_1), 7.5)
 
 class ZScore(unittest.TestCase):
     def test_population_z_score(self):
         my_data = [10, 20, 12, 17, 16]
-        my_set = DescriptiveStats(my_data)
-        self.assertAlmostEqual(my_set.population_z_score(0), -1.3975, 4)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertAlmostEqual(my_set.population_z_score(0, data=my_set.data_1), -1.3975, 4)
     def test_sample_z_score(self):
         my_data = [10, 20, 12, 17, 16]
-        my_set = DescriptiveStats(my_data)
-        self.assertEqual(my_set.sample_z_score(0), -1.25)
+        my_set = DescriptiveStats(data_1=my_data)
+        self.assertEqual(my_set.sample_z_score(0, data=my_set.data_1), -1.25)
 
 unittest.main()
